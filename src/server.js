aa1791ea2a9c9d9e9d7940bfcc4f7fac205f0f07
Wallacy Rezende <wallacyrezende@gmail.com>
2021-04-01 19:36:22 -0300

    Real time with socket.io server

--- src/server.js
+++ src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-l7tkn.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -18,9 +23,20 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-l7tkn.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+// app.use(cors({ origin:'http://localhost:3000'}));
+const connectedUsers = {}
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;
+});
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+})
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
